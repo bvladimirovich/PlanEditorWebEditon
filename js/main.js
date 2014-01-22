2ac022c3509057946cb91ffe30f6b638b3400b4f
Boris <ch.b.vladimirovich@gmail.com>
2014-01-22 23:09:35 +0400

    fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 ﻿var elements = {};
-elements[1] = new Struct().set(1,'element1',1,0,1, 2,0,2);
-elements[2] = new Struct().set(2,'element2',4,0,1, 1,0,2);
-elements[3] = new Struct().set(3,'element3',1,0,4, 4,0,1);
+elements[1] = new Struct().set(1,'A',1,0,1, 2,0,2);
+elements[2] = new Struct().set(2,'B',4,0,1, 1,0,2);
+elements[3] = new Struct().set(3,'C',1,0,4, 4,0,1);
+elements[4] = new Struct().set(4,'D',6,0,1, 4,0,4);
+elements[5] = new Struct().set(5,'E',9,0,2, 3,0,2);
+elements[6] = new Struct().set(6,'F',7,0,2, 1,0,2);
+elements[7] = new Struct().set(7,'G',6,0,4, 1,0,2);
+elements[8] = new Struct().set(8,'H',8,0,4, 1,0,2);
 
 var e = new List();
 e.add(elements[1]);
 e.add(elements[2]);
 e.add(elements[3]);
+e.add(elements[4]);
+e.add(elements[5]);
+e.add(elements[6]);
+e.add(elements[7]);
+e.add(elements[8]);
 
 alert(e.length());
-for(var i in e.getAll()){
-  alert(e.getAll()[i].type);
-}
-
-
--- js/struct.js
+++ js/struct.js
@@ -12,25 +12,46 @@ Struct.prototype.set = function(id,type,x,y,z,lx,ly,lz){
   }
 }
 
-var List = function(){List.counter=0; List.items={}}
-List.prototype.add = function(/*Element*/e){
-  List.items[List.counter] = e;
-  List.counter++;
+var List = function(){
+  List.counter=0;
+  List.items={};
 }
-List.prototype.getAll = function(){
-  return List.items;
+List.prototype.add = function(/*element*/e){
+    var bool = true;
+    for(var i in List.items){
+	  if(isIntersects(e, List.items[i])) {
+	    bool=false;
+		alert('1 '+e.type);
+	  }else if(isIntersects(List.items[i], e)){
+	    bool=false;
+		alert('2 '+e.type);
+	  }else bool=true;
+    }
+	if(bool){
+	  List.items[List.counter] = e;
+      List.counter++;
+	}
 }
-List.prototype.getItem = function(index){
-  return List.items[index];
+List.prototype.getItem = function(/*index*/){
+  if(arguments.length == 1) return List.items[arguments[0]];
+  else return List.items;
 }
 List.prototype.length = function(){
   return List.counter;
 }
+List.prototype.remove = function(){
+  if(arguments.length == 1) delete List.items[arguments[0]];
+  else List.items={};
+}
 
-  for (var i in List){
-    var e = List[i];
-    if (x > e.A['x'] && x < e.C['x'])
-    if (y > e.A['y'] && y < e.C['y'])
-	  return e;
-  }
-  return false;
+var isIntersects = function (a, /*New element*/b) {
+  a.x1 = a.x + a.lx;
+  a.z1 = a.z + a.lz;
+  b.x1 = b.x + b.lx;
+  b.z1 = b.z + b.lz;
+  var res = true;
+  if(b.x1<a.x || b.x>a.x1) res = false
+  if(res != false)
+  if(b.z<a.z || b.z1>a.z1) res = false;
+  return res;
+}
