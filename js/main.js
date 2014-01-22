0a4a37f95adf77889b1918b667527a2c716c0c9a
Chirkov Boris <ch.b.vladimirovich@gmail.com>
2014-01-22 15:16:41 +0400

    created new file

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿var elements = {};
+elements[1] = new Struct().set(1,'element1',1,0,1, 2,0,2);
+elements[2] = new Struct().set(2,'element2',4,0,1, 1,0,2);
+elements[3] = new Struct().set(3,'element3',1,0,4, 4,0,1);
+
+var e = new List();
+e.add(elements[1]);
+e.add(elements[2]);
+e.add(elements[3]);
+
+alert(e.length());
+for(var i in e.getAll()){
+  alert(e.getAll()[i].type);
+}
+
+
