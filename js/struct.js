7a5ce34c49a8d66e1d5f03b11c49ebd82bb04322
Chirkov Boris <ch.b.vladimirovich@gmail.com>
2014-01-22 15:16:06 +0400

    Новая структура данны�

---
+++ js/struct.js
@@ -0,0 +1,36 @@
+﻿var Struct = function(){}
+Struct.prototype.set = function(id,type,x,y,z,lx,ly,lz){
+  return{
+    id: id,
+	type: type,
+	x: x,
+	y: y,
+	z: z,
+	lx: lx,
+	ly: ly,
+	lz: lz
+  }
+}
+
+var List = function(){List.counter=0; List.items={}}
+List.prototype.add = function(/*Element*/e){
+  List.items[List.counter] = e;
+  List.counter++;
+}
+List.prototype.getAll = function(){
+  return List.items;
+}
+List.prototype.getItem = function(index){
+  return List.items[index];
+}
+List.prototype.length = function(){
+  return List.counter;
+}
+
+  for (var i in List){
+    var e = List[i];
+    if (x > e.A['x'] && x < e.C['x'])
+    if (y > e.A['y'] && y < e.C['y'])
+	  return e;
+  }
+  return false;
