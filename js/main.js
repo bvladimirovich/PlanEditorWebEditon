3ce74a24e612061cc083fe38090ce1031c068a96
Boris <ch.b.vladimirovich@gmail.com>
2014-01-24 22:00:59 +0400

    change

--- js/main.js
+++ js/main.js
@@ -1,37 +1,13 @@
-var elements = {};
-var a = new Struct().set(1, 'a' ,0,0,0, 3,1,3);
-var b1 = new Struct().set(2,'b1',5,0,0, 4,1,5);
-var b2 = new Struct().set(3,'b2',12,0,0, 4,1,2);
-                               
-var e = new List();
-
-console.log('Длина списка '+e.length());
-e.add(a);
-e.add(b1);
-e.add(b2);
-console.log('Длина списка '+e.length());
-
-var s = new Section();
-var ab1 = s.get(e.get(0), e.get(1), e.get());
-var ab2 = s.get(e.get(0), e.get(2), e.get());
-console.log('*********');
-console.log(ab1);
-console.log(ab2);
-console.log('*********');
-console.log(s.get(e.get(2), e.get(0), e.get()));
-console.log(s.get(e.get(2), e.get(1), e.get()));
-console.log('*********');
-console.log(s.get(e.get(1), e.get(0), e.get()));
-console.log(s.get(e.get(1), e.get(2), e.get()));
-console.log('*********');
-console.log('Дано');
-console.log(a);
-console.log(b1);
-console.log(b2);
-console.log('Дано2');
 var build = new Building();
-var s1 = build.addSpace('s1',0,0,0, 4,1,2);
-var s2 = build.addSpace('s2',7,0,0, 4,1,5);
-console.log(s1);
-console.log(s2);
-build.addDoor(s1, s2);
+console.log('Добавление комнат');
+var r1 = build.addRoom(0,0,0, 3,1,3)
+var r2 = build.addRoom(4,0,0, 4,1,4)
+var r3 = build.addRoom(15,0,0, 2,1,2)
+console.log(r1);
+console.log(r2);
+console.log(r3);
+console.log('Добавление двери между комнатами');
+console.log(build.addDoor(r2, r1));
+console.log('Изменение размеров');
+build.modify(0).size(3,2,3);
+console.log(build.getItem(0));
