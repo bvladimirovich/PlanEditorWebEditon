9291457b2a8378051810eea0ed113f6a8c1a5564
Chirkov Boris <ch.b.vladimirovich@gmail.com>
2014-01-28 19:47:55 +0400

    added function wheel and drop
    Установлен зум и перемещение рабочего простанства

--- js/main.js
+++ js/main.js
@@ -1,25 +1,105 @@
-var build = new Building();
-console.log('Добавление комнат');
-var r1 = build.addRoom(0,0,0, 3,1,3)
-var r2 = build.addRoom(4,0,0, 4,1,4)
-var r3 = build.addRoom(0,0,5, 6,1,3)
-console.log(r1);
-console.log(r2);
-console.log(r3);
-console.log('Добавление двери между комнатами');
-var door = build.addDoor(r1, r2, 1,1,1);
-console.log(door);
-console.log('Изменение размеров');
-console.log(build.modify(door.id).position(5,2,-2));
-console.log(build.modify(door.id).position(5,2,-1));
-console.log(build.modify(door.id).position(5,2,0));
-console.log(build.modify(door.id).position(5,2,1));
-console.log(build.modify(door.id).position(5,2,2));
-console.log(build.modify(door.id).position(5,2,0));
-console.log(build.modify(door.id).position(5,2,0));
-console.log(build.modify(door.id).position(5,2,3));
-console.log(build.modify(door.id).position(5,2,2));
-console.log(build.modify(door.id).position(5,2,1));
-console.log(build.modify(door.id).position(5,2,0));
-console.log(build.modify(door.id).position(5,2,-2));
-console.log(build.modify(door.id).position(5,2,-1));
+// var build = new Building();
+// console.log('Добавление комнат');
+// var r1 = build.addRoom(0,0,0, 3,1,3)
+// var r2 = build.addRoom(4,0,0, 4,1,4)
+// var r3 = build.addRoom(0,0,5, 6,1,3)
+// console.log(r1);
+// console.log(r2);
+// console.log(r3);
+// console.log('Добавление двери между комнатами');
+// var door = build.addDoor(r1, r2, 1,1,1);
+// console.log(door);
+// console.log('Изменение размеров');
+// console.log(build.modify(door.id).position(5,2,-2));
+// console.log(build.modify(door.id).position(5,2,-1));
+// console.log(build.modify(door.id).position(5,2,0));
+// console.log(build.modify(door.id).position(5,2,1));
+// console.log(build.modify(door.id).position(5,2,2));
+// console.log(build.modify(door.id).position(5,2,0));
+// console.log(build.modify(door.id).position(5,2,0));
+// console.log(build.modify(door.id).position(5,2,3));
+// console.log(build.modify(door.id).position(5,2,2));
+// console.log(build.modify(door.id).position(5,2,1));
+// console.log(build.modify(door.id).position(5,2,0));
+// console.log(build.modify(door.id).position(5,2,-2));
+// console.log(build.modify(door.id).position(5,2,-1));
+
+
+
+function wheelListener(){
+
+	var elem = document.getElementById('canvas');
+	if (elem.addEventListener) {
+	  if ('onwheel' in document) {
+		elem.addEventListener ("wheel", onWheel, false);
+	  } else if ('onmousewheel' in document) {
+		elem.addEventListener ("mousewheel", onWheel, false);
+	  } else {
+		elem.addEventListener ("MozMousePixelScroll", onWheel, false);
+	  }
+	} else {
+	  elem.attachEvent ("onmousewheel", onWheel);
+	}
+
+	function onWheel(e) {
+		e = e || window.event;
+		var delta = e.deltaY || e.detail || e.wheelDelta;
+		cam.setZoom((delta > 0)?1.1:0.9);
+		drawScene(0.0, 0.0);
+		e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+	}
+}
+
+function dragRight(tool){
+	var selector = '#canvas';
+	$(selector).on("mousedown", s);
+	$(selector).on("mousemove", s);
+	$(selector).on("mouseup", s);
+
+	function s(e) {
+		if (tool[e.type]) {
+			tool[e.type](e);
+		}
+	}
+}
+
+function draggable(func) {
+  var elem = document.getElementById('canvas');
+  // флаг перемещения
+  var drag = false;
+  var dx, dy,
+	prevX, prevY;
+  
+  this.mousedown = function (ev) {
+	// старт перемещения
+	if (!drag) drag = true;
+	prevX = f(ev, 'x');
+	prevY = f(ev, 'y');
+  };
+
+  this.mousemove = function (ev) {
+	if (!drag) return;
+	// координаты в момент перемещения мышки
+	var k = elem.width/(cam.update().r-cam.update().l);
+	var nX = f(ev, 'x');
+	var nY = f(ev, 'y');
+	dx = (nX-prevX)/k;
+	dy = (nY-prevY)/k;
+	prevY = nY;
+	prevX = nX;
+	
+	cam.setDxy(dx, dy);
+	drawScene();
+  };
+
+  this.mouseup = function (ev) {
+	// прекращение перемещения
+	if (drag) drag = false;
+  };
+}
+	
+/* Функция вычисления координаты мыши на холсте */
+function f(ev, p) {
+  var elem = document.getElementById('canvas');
+  return (p=='x')?ev.pageX-elem.offsetLeft:elem.height-(ev.pageY-elem.offsetTop);
+}
