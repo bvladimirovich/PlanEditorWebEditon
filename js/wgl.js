f1164bbbaad0f4e1f463e0aeaeaa1a2040492bb0
Chirkov Boris <ch.b.vladimirovich@gmail.com>
2014-01-28 19:48:45 +0400

    added webGL init file

---
+++ js/wgl.js
@@ -0,0 +1,158 @@
+﻿var gl, cam;
+function initGL(canvas) {
+	try {
+		gl = canvas.getContext("experimental-webgl");
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	} catch (e) {
+	}
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry :-(");
+	}
+	
+	cam = new Camera();
+}
+
+
+function getShader(gl, id) {
+	var shaderScript = document.getElementById(id);
+	if (!shaderScript) {
+		return null;
+	}
+
+	var str = "";
+	var k = shaderScript.firstChild;
+	while (k) {
+		if (k.nodeType == 3) {
+			str += k.textContent;
+		}
+		k = k.nextSibling;
+	}
+
+	var shader;
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		return null;
+	}
+
+	gl.shaderSource(shader, str);
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+
+var shaderProgram;
+
+function initShaders() {
+	var fragmentShader = getShader(gl, "shader-fs");
+	var vertexShader = getShader(gl, "shader-vs");
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		alert("Could not initialise shaders");
+	}
+
+	gl.useProgram(shaderProgram);
+
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+}
+
+
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+function setMatrixUniforms() {
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+var squareVertexPositionBuffer;
+
+function initBuffers() {
+	squareVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+	vertices = [
+		 1.0,  1.0,  0.0,
+		-1.0,  1.0,  0.0,
+		 1.0, -1.0,  0.0,
+		-1.0, -1.0,  0.0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	squareVertexPositionBuffer.itemSize = 3;
+	squareVertexPositionBuffer.numItems = 4;
+}
+
+function drawScene() {
+	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	var dz = cam.getZoom(), 
+		dx = cam.getDx(),
+		dy = cam.getDy();
+		
+	var ia = cam.update().l,
+		ib = cam.update().r,
+		ic = cam.update().b,
+		id = cam.update().t;
+    mat4.ortho(ia, ib, ic, id, 0.1, 100.0, pMatrix);
+	mat4.identity(mvMatrix);
+	
+    mat4.translate(mvMatrix, [0.0, 0.0, -0.1]);	
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+}
+
+var Camera = function () {
+  this.zoom = 1.0;
+  this.dx = 0.0;
+  this.dy = 0.0;
+  
+  this.l = -1.0;
+  this.r = 1.0;
+  this.b = -1.0;
+  this.t = 1.0;
+}
+Camera.prototype.setZoom = function (a) {
+	this.zoom = Math.max(Math.min(this.zoom * a, 30.0), 1.0);
+}
+Camera.prototype.getZoom = function () {
+	return this.zoom;
+}
+Camera.prototype.setDxy = function (a, b) {
+	this.dx -= a;
+	this.dy -= b;
+}
+Camera.prototype.getDx = function () {
+	return this.dx;
+}
+Camera.prototype.getDy = function () {
+	return this.dy;
+}
+Camera.prototype.update = function () {
+	return{
+		l: this.l*this.zoom + this.dx,
+		r: this.r*this.zoom + this.dx,
+		b: this.b*this.zoom + this.dy,
+		t: this.t*this.zoom + this.dy
+	}
+}
+
