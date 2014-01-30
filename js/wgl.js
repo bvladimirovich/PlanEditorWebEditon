4a7c05d6c74ce7e08a05538716c4a48332d99019
Chirkov Boris <ch.b.vladimirovich@gmail.com>
2014-01-30 12:59:49 +0400

    added function initScene()
    Функция initScene() реализует в себе управление камерой и отрисовку
    сцены.

--- js/wgl.js
+++ js/wgl.js
@@ -1,5 +1,4 @@
-﻿var gl, 
-	cam;
+﻿var gl;
 	
 function initGL(canvas) {
 	try {
@@ -11,8 +10,6 @@ function initGL(canvas) {
 	if (!gl) {
 		alert("Could not initialise WebGL, sorry :-(");
 	}
-	
-	cam = new Camera();
 }
 
 function getShader(gl, id) {
@@ -100,11 +97,15 @@ function initBuffers() {
 	squareVertexPositionBuffer.numItems = 4;
 }
 
-function drawScene() {
+function drawScene(cam) {
+	if (cam.constructor != Camera) {
+		throw new Error('Входящий параметр не верного типа. Ожидался экземпляр класса Camera.');
+	}
+	
 	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.ortho(cam.update().l, cam.update().r, cam.update().b, cam.update().t, 0.1, 100.0, pMatrix);
+    mat4.ortho(cam.get().l, cam.get().r, cam.get().b, cam.get().t, 0.1, 100.0, pMatrix);
 	mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, [0.0, 0.0, -0.1]);
 
@@ -114,38 +115,122 @@ function drawScene() {
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
 }
 
-var Camera = function () {
-	this.zoom = 1.0;
-	this.dx = 0.0;
-	this.dy = 0.0;
+/**
+	initScene(elem) - Функция инициализации параметров сцены (рабочего пространства).
+	Входной параметр elem - объект страницы над которым происходит изменение положения колёсика мыши.
+	функция wheelListener() реализует приближение/отдаление сцены.
+	функция dragRight() реализует перемещение сцены правой кнопкой мыши.
+	Переменная cam - объект класса Camera. При создании объекта 
+устанавливаются начальные параметры смещения и приближения сцены.
+	Функция drawScene(cam) отрисовывает сцену. Входной параметр - объект класса Camera.
+*/
+var cam;
 
-	this.l = -1.0;
-	this.r = 1.0;
-	this.b = -1.0;
-	this.t = 1.0;
-}
-Camera.prototype.setZoom = function (a) {
-	this.zoom = Math.max(Math.min(this.zoom * a, 30.0), 1.0);
-}
-Camera.prototype.getZoom = function () {
-	return this.zoom;
-}
-Camera.prototype.setDxy = function (a, b) {
-	this.dx -= a;
-	this.dy -= b;
-}
-Camera.prototype.getDx = function () {
-	return this.dx;
-}
-Camera.prototype.getDy = function () {
-	return this.dy;
-}
-Camera.prototype.update = function () {
-	return{
-		l: this.l*this.zoom + this.dx,
-		r: this.r*this.zoom + this.dx,
-		b: this.b*this.zoom + this.dy,
-		t: this.t*this.zoom + this.dy
+function initScene(elem) {
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+	
+	cam = new Camera({
+		zoom:10.0,
+		dx:0.0,
+		dy:0.0,
+		left:-1.0,
+		right:1.0,
+		bottom:-1.0,
+		top:1.0
+	});
+	
+	wheelListener();
+	dragRight();
+	drawScene(cam);
+	
+	function wheelListener(){
+		if (elem.addEventListener) {
+			if ('onwheel' in document) {
+				elem.addEventListener("wheel", onWheel, false);
+			} else if ('onmousewheel' in document) {
+				elem.addEventListener("mousewheel", onWheel, false);
+			} else {
+				elem.addEventListener("MozMousePixelScroll", onWheel, false);
+			}
+		} else {
+			elem.attachEvent("onmousewheel", onWheel);
+		}
+
+		function onWheel(e) {
+			e = e || window.event;
+			var delta = e.deltaY || e.detail || e.wheelDelta;
+			cam.setZoom((delta > 0)?1.1:0.9);
+			drawScene(cam);
+			e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+		}
+	}
+
+	function dragRight() {
+		var selector = '#canvas';
+		$(selector).on("mousedown", s);
+		$(selector).on("mousemove", s);
+		$(selector).on("mouseup", s);
+		$(selector).on("contextmenu", function () {
+			return false;
+		});
+		
+		var tool = new Draggable();
+		function s(e) {
+			if (tool[e.type]) {
+				tool[e.type](e);
+			}
+		}
+		
+		function Draggable() {
+			var drag = false,
+				prevX,
+				prevY;
+		    
+			this.mousedown = function (ev) {
+				if(!fixWhich(ev)) return;
+				drag = true;
+				prevX = f(ev, 'x');
+				prevY = f(ev, 'y');
+			}
+			this.mousemove = function (ev) {
+				if (!drag) return;
+				var k = gl.viewportWidth / (cam.get().r - cam.get().l),
+					nX = f(ev, 'x'),
+					nY = f(ev, 'y'),
+					dx = (nX-prevX)/k,
+					dy = (nY-prevY)/k;
+
+				prevY = nY;
+				prevX = nX;
+
+				cam.setDxDy(dx, dy);
+				drawScene(cam);
+			}
+			this.mouseup = function (ev) {
+				if (drag) drag = false;
+			}
+		}
+			
+		/* Функция вычисления координаты мыши на холсте */
+		function f(ev, p) {
+		  var elem = document.getElementById('canvas');
+		  return (p=='x') ? ev.pageX-elem.offsetLeft : elem.height-(ev.pageY-elem.offsetTop);
+		}
+		/* Функция определения нажатой на мыши кнопки */
+		function fixWhich(e) {
+			if (!e.which && e.button) { // если which нет, но есть button...
+				if (e.button & 1) { 
+					e.which = 1;        // левая кнопка
+				} else if (e.button & 4) {
+					e.which = 2; 		// средняя кнопка
+				} else if (e.button & 2) {
+					e.which = 3; 		// правая кнопка
+				}
+			}
+			if (e.which == 3) {
+				return true;
+			}
+		}
 	}
 }
-
